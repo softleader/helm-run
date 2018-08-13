4c72a087bd92ae6120f0c014d16ea2dc2940fbd9
Matt S.Y. Ho <methodho@gmail.com>
2018-08-13 14:43:24 +0800

    update

--- install-binary.sh
+++ install-binary.sh
@@ -94,7 +94,6 @@ installFile() {
   tar xf "$PLUGIN_TMP_FILE" -C "$HELM_TMP"
   HELM_TMP_BIN="$HELM_TMP/run"
   echo "Preparing to install into ${HELM_PLUGIN_PATH}"
-  mkdir -p "$HELM_PLUGIN_PATH"
   # Use * to also copy the file withe the exe suffix on Windows
   cp "$HELM_TMP_BIN"* "$HELM_PLUGIN_PATH"
   # Copy the plugin.yaml as well
