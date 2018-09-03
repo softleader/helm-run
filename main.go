a6b44166b27391838a115fc6decf366da7c39fce
matt <methodho@gmail.com>
2018-09-03 14:59:11 +0800

    add shorten flag

--- main.go
+++ main.go
@@ -34,11 +34,11 @@ func main() {
 		},
 	}
 	f := cmd.Flags()
-	f.StringVarP(&runCmd.image, "image", "", "softleader/helm", "image for running command")
+	f.StringVarP(&runCmd.image, "image", "i", "softleader/helm", "image for running command")
 	f.BoolVarP(&runCmd.alwaysPullImage, "always-pull-image", "", false, "always pull image before running command")
 	f.BoolVarP(&runCmd.rm, "rm", "", true, "automatically remove the container when it exits")
 	f.StringArrayVarP(&runCmd.entryPoint, "entrypoint", "", []string{"/bin/bash"}, "the ENTRYPOINT of the image")
-	f.BoolVarP(&runCmd.local, "local", "", false, "command store on local storage, not on github")
+	f.BoolVarP(&runCmd.local, "local", "l", false, "command store on local storage, not on github")
 	f.BoolVarP(&runCmd.dos2unix, "dos2unix", "", true, "convert FILE from DOS to Unix format")
 	f.StringVarP(&runCmd.commandOwner, "command-owner", "o", commandOwner, "github owner of command")
 	f.StringVarP(&runCmd.commandRepo, "command-repo", "r", commandRepo, "github repo of command")
